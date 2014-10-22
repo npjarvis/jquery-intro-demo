33d7016d7e230a345ace97fc05ea3150ebfbecd2
nathan <npjarvis@uw.edu>
2014-10-22 12:20:42 -0700

    second half of class

--- js/app.js
+++ js/app.js
@@ -4,21 +4,57 @@
 
 "use strict";
 
-jQuery(document).ready(function () {
+$(document).ready(function () {
     // same syntax you would use in CSS to make a rule for all hyperlinks
-    jQuery('p a[href!="#top"]').attr('target', '_blank');
+    $('p a[href!="#top"]').attr('target', '_blank');
     //to target specific paragraphs use jQuery('#sec1 p').
-    jQuery('section').hide().slideDown(1000);
+    $('section').hide().slideDown(1000);
 
     // .click() is an event listener in jQuery
-    jQuery('nav a, p a[href="#top"]').click(function(eventObject) {
+    $('nav a, p a[href="#top"]').click(function(eventObject) {
         //keyword this refers to element that raised the event
         // console.log allows us to see whats happening
         console.log(this.hash);
         var targetElement = jQuery(this.hash);
         //scrolling example: firefox and chrome use different elements for scrolls, such as html,body
-        jQuery('html,body').animate({scrollTop: targetElement.offset().top}, 700);
+        $('html,body').animate({
+            // set scroll top value to targetElement.offset().top - navHeight
+            scrollTop: targetElement.offset().top - navHeight
+
+        }, 700);
 
         eventObject.preventDefault();
     });
+
+    var nav = $('nav');
+    var navTop = nav.offset().top;
+    var navHeight = nav.outerHeight();
+    var navPlaceHolder = $('.nav-placeholder');
+    navPlaceHolder.height(navHeight);
+
+    // "scrolling" event happens on window object
+
+    $(window).scroll(function () {
+       var scrollPos = $(this).scrollTop();
+       //console.log(scrollPos)
+        if (scrollPos > navTop) {
+            //nav-fixed is a class defined in main.css
+            nav.addClass('nav-fixed');
+            navPlaceHolder.show();
+        }
+        else {
+            nav.removeClass('nav-fixed');
+            navPlaceHolder.hide();
+        }
+    });
+    // type in button you want the click event on
+    $('#exit-button').click(function () {
+       // must get reference to div in html that references the modal
+        $('#confirm-exit-modal').modal();
+    });
+
+    $('#confirm-exit-button').click(function () {
+        window.location ='https://ischool.uw.edu';
+    });
+
 });
